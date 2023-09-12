dc9213e3437ea847c4ec5e70c670deeecaa4cc3d
Daniel <daniel.furtuna@assist.ro>
2023-09-12 10:20:25 +0300

    [t] - try again

--- cypress/pageObjects/journeys/journeyBuilder/components/configPanel/queryBuilder/queryBuilder.ts
+++ cypress/pageObjects/journeys/journeyBuilder/components/configPanel/queryBuilder/queryBuilder.ts
@@ -30,7 +30,7 @@ class QueryBuilder {
   }
 
   get GroupContainer() {
-    return cy.get(this.groupContainer)
+    return cy.get(this.groupContainer).should('be.visible')
   }
 
   get NonStringField() {
